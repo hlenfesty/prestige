4137c6ff8c2a10876e87923499f8fafaba35269c
thomasmorgan <thomas.j.h.morgan@gmail.com>
2018-03-22 11:24:49 -0700

    toms model

--- run.py
+++ run.py
@@ -1,83 +1,89 @@
+''' PARAMETERS '''
+
+# how many repeat models
+repeats = 1
+# how many steps per model
+steps = 100
+# population size
+N = 400
+# size of the world
+width = 20
+height = 20
+# is it a torroidal world?
+donut = True
+# how far away agents count as neighbors
+neighbor_distance = 3
+
+''' SIMULATION '''
+
+# imports
 import matplotlib.pyplot as plt
-from pmodel_6 import PrestigeModel
+from toms_model import PrestigeModel
 import numpy as np
 
-all_copies = []
-all_sigmas = []
-all_sigmas_local = []
-all_sigmas_global = []
+# create empty arrays
+all_copies = np.array([], dtype=int)
+all_sigmas_local = np.array([], dtype=float)
+all_sigmas_global = np.array([], dtype=float)
 models = []
-steps = 100 #steps
 
-for j in range(1):
-    #Run the model
-    models.append(PrestigeModel(1600, 40, 40, 3))
+# create and run models
+for j in range(repeats):
+    models.append(PrestigeModel(N, width, height, donut, neighbor_distance))
     for i in range(steps):
         models[j].step()
 
-    for agent in models[j].schedule.agents:
-        all_copies.append(agent.copies)
-        all_sigmas.append(agent.sigma_ratio)
-        all_sigmas_local.append(agent.sigma_local)
-        all_sigmas_global.append(agent.sigma_global)
+''' PLOT FIGURES '''
 
-
-#plot sigmas over time
+# plot sigmas over time
 plt.figure("variation over time")
-model = models[0] #get the first model only
-agents = model.schedule.agents # get the list of agents from the scheduler from the model
-avg_sigmas_local = np.zeros(steps) # make an empty array of zeros that is the length of the generations/n of agents, call is "avg_sigma_local"
+model = models[0]  # get the first model only
+agents = model.agents  # get the list of agents from the scheduler from the model
+avg_sigmas_local = agents['sigma_local_history'].mean(axis=1)
+avg_sigmas_global = agents['sigma_global_history'].mean(axis=1)
 
-for a in agents: 
-    avg_sigmas_local += np.array(a.sigma_local_history) #get each agents' local sigma history, and add up the columns to the array of zeros
-
-avg_sigmas_local /= model.num_agents #divide the summed list of agents' histories (added to zeros array) by num agents
-
-avg_sigmas_global = np.zeros(steps)
-
-for a in agents:
-    avg_sigmas_global += np.array(a.sigma_global_history)
-
-avg_sigmas_global /= model.num_agents
-
-time = range(steps)
-plt.plot(time, avg_sigmas_local, color = 'orange')
-plt.plot(time, avg_sigmas_global, color = 'blue')
+time = range(steps + 1)
+plt.plot(time, avg_sigmas_local, color='orange')
+plt.plot(time, avg_sigmas_global, color='blue')
 plt.xlabel('time')
 plt.ylabel('local vs global variation')
-plt.title ('local vs global variation over time')
+plt.title('local vs global variation over time')
 
-#plot the frequency of N copies
+# #plot the frequency of N copies
+
+for model in models:
+    all_copies = np.append(all_copies, model.agents['copied'])
 
 plt.figure("copies")
-plt.hist(all_copies, bins=range(max(all_copies)+1))
+plt.hist(all_copies)
 
 #figure 2 plot the frequency of belief types
 
 plt.figure("beliefs")
 model = models[0]
-agents = model.schedule.agents  # get a list of the agents, that's already stored in the scheduler
-beliefs = [a.belief for a in agents]  # get a list of the end beliefs
-plt.hist(beliefs, bins=range(max(beliefs)+1))
+beliefs = model.agents['belief']  # get a list of the end beliefs
+plt.hist(beliefs)
 
 #plot beliefs and N copies together
 
 model = models[0]
-agents = model.schedule.agents  # get a list of the agents, that's already stored in the scheduler
-agents_pos = [a.pos for a in agents]
-xs = [x[0] for x in agents_pos]
-ys = [y[1] for y in agents_pos]
-colors = [a.belief for a in agents]  # get a list of the end beliefs
-area = [c.copies for c in agents]  # area will scale to N of copies
+xs = model.agents['x']
+ys = model.agents['y']
+colors = model.agents['belief']
+area = model.agents['copied']
 
 plt.figure("heatmap")
 plt.scatter(xs, ys, s=area, c=colors, alpha=.5)  # alpha is transparency
 
-#plot histogram of sigmas
+# #plot histogram of sigmas
 
-plt.figure("variance")
-time = models[0]
-sigmas = all_sigmas
-plt.hist(all_sigmas)
+for model in models:
+    all_sigmas_local = np.append(all_sigmas_local, model.agents['sigma_local'])
+    all_sigmas_global = np.append(all_sigmas_global, model.agents['sigma_global'])
+
+plt.figure("global variance")
+plt.hist(all_sigmas_global)
+plt.figure("local variance")
+plt.hist(all_sigmas_local)
+
 plt.show()
-
---
+++ toms_model.py
@@ -0,0 +1,112 @@
+import numpy as np
+from random import shuffle
+
+
+class PrestigeModel():
+    """A model of prestiged biased copying."""
+
+    def __init__(self, N, width, height, donut, neighbor_distance):
+        # initialize the model
+        self.num_agents = N
+        self.width = width
+        self.height = height
+        self.donut = donut
+        self.neighbor_distance = neighbor_distance
+
+        # agents are represented with a dictionary of arrays
+        self.agents = {
+            # the id of the agent
+            'id': np.empty(shape=(self.num_agents), dtype=int),
+            # the agent's x coordinate
+            'x': np.empty(shape=(self.num_agents), dtype=int),
+            # the agent's y coordinate
+            'y': np.empty(shape=(self.num_agents), dtype=int),
+            # a matrix of distances from agent i to agent j
+            'distance': np.empty(shape=(self.num_agents, self.num_agents)),
+            # the beliefs of each agent
+            'belief': np.empty(shape=(self.num_agents), dtype=int),
+            # the beliefs of each agent at each generation
+            'belief_history': np.empty(shape=(self.num_agents), dtype=int),
+            # the number of times each agent has been copied
+            'copied': np.zeros(shape=(self.num_agents), dtype=int),
+            # the number of times each agent had been coped at each generation
+            'copied_history': np.zeros(shape=(self.num_agents), dtype=int),
+            # the proportion of agents that have different beliefs to you
+            'sigma_global': np.ones(shape=(self.num_agents), dtype=float),
+            # the proportion of agents that had different beliefs to you at each generation
+            'sigma_global_history': np.ones(shape=(self.num_agents), dtype=float),
+            # the proportion of local agents that have different beliefs to you
+            'sigma_local': np.ones(shape=(self.num_agents), dtype=float),
+            # the propotion of local agents that had different beliefs to you at each generation
+            'sigma_local_history': np.ones(shape=(self.num_agents), dtype=float)
+        }
+
+        # Create the agents
+        # simply updates the id, x, y and belief values of the agent dictionary
+        for i in range(self.num_agents):
+            self.agents['id'][i] = round(i)
+            self.agents['x'][i] = round((i % self.width))
+            self.agents['y'][i] = round((i // self.height))
+            self.agents['belief'][i] = round(i)
+
+        # add the beliefs as the first row of the belief history matrix
+        self.agents['belief_history'] = self.agents['belief']
+
+        # create a distance matrix
+        for i in range(self.num_agents):
+            x = self.agents['x'][i]
+            y = self.agents['y'][i]
+            dx = np.absolute(x - self.agents['x'])
+            dy = np.absolute(y - self.agents['y'])
+
+            # if the world is toroidal its a little more complicated
+            if self.donut:
+                dx2 = self.width - dx
+                dy2 = self.height - dy
+                dx = np.minimum(dx, dx2)
+                dy = np.minimum(dy, dy2)
+
+            dist = (dx**2 + dy**2)**0.5
+            self.agents['distance'][i, :] = dist
+
+    def step(self):
+        '''Advance the model by one step.'''
+
+        # randomize the order of agents
+        indexes = list(range(self.num_agents))
+        shuffle(indexes)
+        for i in indexes:
+
+            # pick who to copy
+            probs = (self.agents['copied']+1)*np.exp(-self.agents['distance'][i, :]**2)
+            probs = probs / sum(probs)
+            other_agent = list(range(self.num_agents))[np.random.multinomial(1, probs).argmax()]
+
+            # if they aren't yourself, copy them
+            if i != other_agent:
+                self.agents['copied'][other_agent] = self.agents['copied'][other_agent] + 1
+                self.agents['belief'][i] = self.agents['belief'][other_agent]
+
+        # calculate sigma global and sigma local
+        sigma_global = np.zeros(shape=(self.num_agents), dtype=float)
+        sigma_local = np.zeros(shape=(self.num_agents), dtype=float)
+
+        for i in list(range(self.num_agents)):
+            # sigma global
+            sigma_global[i] = np.mean(self.agents['belief'] != self.agents['belief'][i])
+            # sigma local
+            local_beliefs = np.array([b for b, d in zip(self.agents['belief'], self.agents['distance'][i, :]) if d <= self.neighbor_distance and d > 0])
+            sigma_local[i] = np.mean(local_beliefs != self.agents['belief'][i])
+
+        # this line takes into account that sigma global includes yourself and so removes you from the calculation
+        sigma_global = (sigma_global)/(1-1/self.num_agents)
+
+        # save it
+        self.agents['sigma_global'] = sigma_global
+        self.agents['sigma_local'] = sigma_local
+
+        # save everything to history tables
+        self.agents['belief_history'] = np.vstack((self.agents['belief_history'], self.agents['belief']))
+        self.agents['copied_history'] = np.vstack((self.agents['copied_history'], self.agents['copied']))
+        self.agents['sigma_global_history'] = np.vstack((self.agents['sigma_global_history'], self.agents['sigma_global']))
+        self.agents['sigma_local_history'] = np.vstack((self.agents['sigma_local_history'], self.agents['sigma_local']))
