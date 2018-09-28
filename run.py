a6e06594df2b10b1f9754a3892ee2450e9754e65
thomasmorgan <thomas.j.h.morgan@gmail.com>
2018-09-28 15:51:33 -0700

    add data dict to run.py
    this adds new rows to the data dict dynamically

--- run.py
+++ run.py
@@ -47,6 +47,58 @@ models = []
 #for N in (range(100))
 
 
+data_dict = {
+    'num_agents': np.array([], dtype = int),
+    'width' : np.array([], dtype = int),
+    'height' : np.array([], dtype = int),
+    'donut' : np.array([], dtype = int),
+    'neighbor_distance' : np.array([], dtype = int),
+    'innovate' : np.array([], dtype = float),
+    'population' : np.array([], dtype = str), #need to change string to int index 
+    'exponent' : np.array([], dtype = float),
+    'distance_penalty' : np.array([], dtype = float),
+    'sigma_local' : np.array([], dtype = float),
+    'sigma_global' : np.array([], dtype = float),
+    'sigma_ratio' : np.array([], dtype = float),
+    'corr_sigloc_avgdist' : np.array([], dtype = float),
+    'corr_copied_avgdist' : np.array([], dtype = float),
+    'corr_beliefchg_avgdist' : np.array([], dtype = float),
+    'mean' : np.array([], dtype = float),
+    'median' : np.array([], dtype = float),
+    'mode' : np.array([], dtype = float),
+    'SD' : np.array([], dtype = float),
+    'Pearson_1' : np.array([], dtype = float),
+    'Pearson_2' : np.array([], dtype = float),
+    'Pearson_3' : np.array([], dtype = float),
+    'gini' : np.array([], dtype = float),
+}
+
+def save_model_results(model):
+    data_dict['num_agents'] = np.append(data_dict['num_agents'], model.num_agents)
+    data_dict['width'] = np.append(data_dict['width'], model.num_agents)
+    data_dict['height'] = np.append(data_dict['height'], model.num_agents)
+    data_dict['donut'] = np.append(data_dict['donut'], model.num_agents)
+    data_dict['neighbor_distance'] = np.append(data_dict['neighbor_distance'], model.num_agents)
+    data_dict['innovate'] = np.append(data_dict['innovate'], model.num_agents)
+    data_dict['population'] = np.append(data_dict['population'], model.num_agents)
+    data_dict['exponent'] = np.append(data_dict['exponent'], model.num_agents)
+    data_dict['distance_penalty'] = np.append(data_dict['distance_penalty'], model.num_agents)
+    data_dict['sigma_local'] = np.append(data_dict['sigma_local'], model.num_agents)
+    data_dict['sigma_global'] = np.append(data_dict['sigma_global'], model.num_agents)
+    data_dict['sigma_ratio'] = np.append(data_dict['sigma_ratio'], model.num_agents)
+    data_dict['corr_sigloc_avgdist'] = np.append(data_dict['corr_sigloc_avgdist'], model.num_agents)
+    data_dict['corr_copied_avgdist'] = np.append(data_dict['corr_copied_avgdist'], model.num_agents)
+    data_dict['corr_beliefchg_avgdist'] = np.append(data_dict['corr_beliefchg_avgdist'], model.num_agents)
+    data_dict['mean'] = np.append(data_dict['mean'], model.num_agents)
+    data_dict['median'] = np.append(data_dict['median'], model.num_agents)
+    data_dict['mode'] = np.append(data_dict['mode'], model.num_agents)
+    data_dict['SD'] = np.append(data_dict['SD'], model.num_agents)
+    data_dict['Pearson_1'] = np.append(data_dict['Pearson_1'], model.num_agents)
+    data_dict['Pearson_2'] = np.append(data_dict['Pearson_2'], model.num_agents)
+    data_dict['Pearson_3'] = np.append(data_dict['Pearson_3'], model.num_agents)
+    data_dict['gini'] = np.append(data_dict['gini'], model.num_agents)
+
+
 # create and run models
 # add 'for' layers here to vary other parameters e.g. exp 1:4
 for N in Ns:
@@ -58,6 +110,13 @@ for N in Ns:
                         model = PrestigeModel(N, width, height, donut, neighbor_distance, innovate, population, exponent, distance_penalty, sigmas)
                         for i in range(steps):
                             model.step()
+                        save_model_results(model)
+
+filename = 'data'
+outfile = open('data', 'wb')
+pickle.dump(data_dict, outfile)
+outfile.close()
+
         #print(models[-1].agents["belief_history"])
 
 #save the output of the model by 'pickling' the agent dictionary
