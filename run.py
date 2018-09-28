b672ff239b21d1da6d29e795371a986c854f8bf3
thomasmorgan <thomas.j.h.morgan@gmail.com>
2018-09-28 15:55:30 -0700

    optionally plot figures

--- run.py
+++ run.py
@@ -27,6 +27,8 @@ sigmas = False
 save_movie = False
 # save the model objects?
 save_models = False
+# plot figures? (only applies to final model run)
+plot_figures = True
 
 
 ''' SIMULATION '''
@@ -45,9 +47,6 @@ all_copies = np.array([], dtype=int)
 all_sigmas_local = np.array([], dtype=float)
 all_sigmas_global = np.array([], dtype=float)
 models = []
-
-#for N in (range(100))
-
 
 data_dict = {
     'num_agents': np.array([], dtype = int),
@@ -114,6 +113,8 @@ for N in Ns:
                             model.step()
                         if save models:
                             models.append(model)
+                        else:
+                            models = [model]
                         save_model_results(model)
 
 filename = 'data'
@@ -130,116 +131,108 @@ if save_models:
 
 
 ''' PLOT FIGURES '''
+if plot_figures:
+    if sigmas:
 
-if sigmas:
+        # plot sigmas over time
+        plt.figure("variation over time")
+        model = models[0]  # get the first model only
+        agents = model.agents  # get the list of agents from the scheduler from the model
+        avg_sigmas_local = agents['sigma_local_history'].mean(axis=1)
+        avg_sigmas_global = agents['sigma_global_history'].mean(axis=1)
 
-    # plot sigmas over time
-    plt.figure("variation over time")
-    model = models[0]  # get the first model only
-    agents = model.agents  # get the list of agents from the scheduler from the model
-    avg_sigmas_local = agents['sigma_local_history'].mean(axis=1)
-    avg_sigmas_global = agents['sigma_global_history'].mean(axis=1)
+        time = range(steps + 1)
+        plt.plot(time, avg_sigmas_local, color='orange')
+        plt.plot(time, avg_sigmas_global, color='blue')
+        plt.xlabel('time')
+        plt.ylabel('local vs global variation')
+        plt.title('local vs global variation over time')
 
-    time = range(steps + 1)
-    plt.plot(time, avg_sigmas_local, color='orange')
-    plt.plot(time, avg_sigmas_global, color='blue')
-    plt.xlabel('time')
-    plt.ylabel('local vs global variation')
-    plt.title('local vs global variation over time')
+        # #plot histogram of sigmas
 
-    # #plot histogram of sigmas
+        for model in models:
+            all_sigmas_local = np.append(all_sigmas_local, model.agents['sigma_local'])
+            all_sigmas_global = np.append(all_sigmas_global, model.agents['sigma_global'])
+
+        plt.figure("global variance")
+        plt.hist(all_sigmas_global)
+        plt.figure("local variance")
+        plt.hist(all_sigmas_local)
+
+    # #plot the frequency of N copies
 
     for model in models:
-        all_sigmas_local = np.append(all_sigmas_local, model.agents['sigma_local'])
-        all_sigmas_global = np.append(all_sigmas_global, model.agents['sigma_global'])
+        all_copies = np.append(all_copies, model.agents['copied'])
 
-    plt.figure("global variance")
-    plt.hist(all_sigmas_global)
-    plt.figure("local variance")
-    plt.hist(all_sigmas_local)
+    plt.figure("copies")
+    plt.hist(all_copies)
 
-# #plot the frequency of N copies
+    #figure 2 plot the frequency of belief types
 
-for model in models:
-    all_copies = np.append(all_copies, model.agents['copied'])
+    plt.figure("beliefs")
+    model = models[0]
+    beliefs = model.agents['belief']  # get a list of the end beliefs
+    plt.hist(beliefs)
 
-plt.figure("copies")
-plt.hist(all_copies)
+    #plot beliefs and N copies together
 
-#figure 2 plot the frequency of belief types
-
-plt.figure("beliefs")
-model = models[0]
-beliefs = model.agents['belief']  # get a list of the end beliefs
-plt.hist(beliefs)
-
-#plot beliefs and N copies together
-
-model = models[0]
-xs = model.agents['x']
-ys = model.agents['y']
-colors = model.agents['belief']
-area = model.agents['copied'] + 1 #makes the littlest dots visible
+    model = models[0]
+    xs = model.agents['x']
+    ys = model.agents['y']
+    colors = model.agents['belief']
+    area = model.agents['copied'] + 1 #makes the littlest dots visible
 
 
-plt.figure("heatmap")
-plt.scatter(xs, ys, s=area, c=colors, cmap='cool', alpha=0.5)
-cbar = plt.colorbar()
+    plt.figure("heatmap")
+    plt.scatter(xs, ys, s=area, c=colors, cmap='cool', alpha=0.5)
+    cbar = plt.colorbar()
 
-plt.title("Bubble plot of Beliefs and Prestige: 'Big Man Societies in Villages'")
-plt.show()
-
-
-# try making a video
-
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+    plt.title("Bubble plot of Beliefs and Prestige: 'Big Man Societies in Villages'")
+    plt.show()
 
 
-class AnimatedScatter(object):
-    """An animated scatter plot using matplotlib.animations.FuncAnimation."""
-    def __init__(self):
-        self.numpoints = N
+    # try making a video
 
-        # Setup the figure and axes...
-        self.fig, self.ax = plt.subplots()
-        # Then setup FuncAnimation.
-        self.ani = animation.FuncAnimation(self.fig, self.update, frames=steps+1, interval=125,
-                                           init_func=self.setup_plot, blit=True)
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
 
 
-    def setup_plot(self):
-        """Initial drawing of the scatter plot."""
-        x, y, s, c = model.agents['x'], model.agents['y'], model.agents['copied_history'][0, :]+1, model.agents['belief_history'][0, :]
-        self.scat = self.ax.scatter(x, y, c=c, cmap='cool', s=s, animated=True)
-        return self.scat,
+    class AnimatedScatter(object):
+        """An animated scatter plot using matplotlib.animations.FuncAnimation."""
+        def __init__(self):
+            self.numpoints = N
+
+            # Setup the figure and axes...
+            self.fig, self.ax = plt.subplots()
+            # Then setup FuncAnimation.
+            self.ani = animation.FuncAnimation(self.fig, self.update, frames=steps+1, interval=125,
+                                               init_func=self.setup_plot, blit=True)
 
 
-    def update(self, i):
-        """Update the scatter plot."""
-        s, c = model.agents['copied_history'][i, :]+1, model.agents['belief_history'][i, :]
-
-        # Set sizes...
-        self.scat._sizes = s
-        # Set colors..
-        self.scat.set_array(c)
-        self.scat._alpha = 0.5
-
-        return self.scat,
-
-a = AnimatedScatter()
-if save_movie:
-
-    a.ani.save('clip.mp4', writer='ffmpeg')
-
-plt.title("Evolution of Prestige-Based Copying")
-plt.show()
+        def setup_plot(self):
+            """Initial drawing of the scatter plot."""
+            x, y, s, c = model.agents['x'], model.agents['y'], model.agents['copied_history'][0, :]+1, model.agents['belief_history'][0, :]
+            self.scat = self.ax.scatter(x, y, c=c, cmap='cool', s=s, animated=True)
+            return self.scat,
 
 
+        def update(self, i):
+            """Update the scatter plot."""
+            s, c = model.agents['copied_history'][i, :]+1, model.agents['belief_history'][i, :]
 
+            # Set sizes...
+            self.scat._sizes = s
+            # Set colors..
+            self.scat.set_array(c)
+            self.scat._alpha = 0.5
 
+            return self.scat,
 
+    a = AnimatedScatter()
+    if save_movie:
 
+        a.ani.save('clip.mp4', writer='ffmpeg')
 
-
+    plt.title("Evolution of Prestige-Based Copying")
+    plt.show()
 
